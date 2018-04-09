33cfd551e7974419e7e3d252e5e466c70da2e1bd
marat-landed <mrtland7@gmail.com>
2018-04-09 09:23:39 +0300

    09-04-2018 09:23

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || 'mongodb://localhost/db_nodejs_ex',
+    mongoURL = 'mongodb://localhost/db_nodejs_ex' || process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
 //console.log("port:",port,"ip:",ip,"mongoURL:",mongoURL,"mongoURLLabel:",mongoURLLabel);
