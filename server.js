ddfd009ad1737a066687edb61540643bc644ab71
marat-landed <mrtland7@gmail.com>
2018-04-10 11:19:43 +0300

    10-04-2018 11:19

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	//var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    // ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-	ipaddr = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+    ipaddr   = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+	//ipaddr = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
