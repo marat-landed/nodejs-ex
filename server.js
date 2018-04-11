b73098556e4a3d331566a003d743add13e535318
marat-landed <mrtland7@gmail.com>
2018-04-11 21:18:54 +0300

    11-04-2018 21:18

--- server.js
+++ server.js
@@ -9,16 +9,23 @@ app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+	//var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ipaddr   = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+	//ipaddr = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
+
+
+
+//console.log("port:",port,"ip:",ip,"mongoURL:",mongoURL,"mongoURLLabel:",mongoURLLabel);
+//console.log("process.env:",process.env);
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
       mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
       mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
       mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
       mongoUser = process.env[mongoServiceName + '_USER'];
 
   if (mongoHost && mongoPort && mongoDatabase) {
@@ -31,13 +38,47 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
     mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
 
   }
+} else if (mongoURL == null && process.env.DATABASE_SERVICE_NAME == null) {
+  mongoURL = 'mongodb://localhost/db_nodejs_ex';
 }
+
+/*
+//mongodb configuration
+var mongoHost = process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost';
+var mongoPort = process.env.OPENSHIFT_MONGODB_DB_PORT || 27017;
+var mongoUser = ''; //mongodb username
+var mongoPass = ''; //mongodb password
+var mongoDb   = ''; //mongodb database name
+
+//mysql configuration
+var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost';
+var mysqlPort = process.env.OPENSHIFT_MYSQL_DB_PORT || 3306;
+var mysqlUser = ''; //mysql username
+var mysqlPass = ''; //mysql password
+var mysqlDb   = ''; //mysql database name
+
+//connection strings
+var mongoString = 'mongodb://' + mongoUser + ':' + mongoPass + '@' + mongoHost + ':' + mongoPort + '/' + mongoDb;
+var mysqlString = 'mysql://'   + mysqlUser + ':' + mysqlPass + '@' + mysqlHost + ':' + mysqlPort + '/' + mysqlDb;
+
+//connect to mongo
+*/
+
+// 10-04-18 Работает с mongoURL: mongodb://userBYJ:EGJVTNgpHIAYTnCe@172.30.201.170:27017/sampledb
+// mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+// mongoUser = process.env[mongoServiceName + '_USER'];
+// mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
+// mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
+// mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
+// mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
+
+
 var db = null,
     dbDetails = new Object();
 
 var initDb = function(callback) {
   if (mongoURL == null) return;
-
+  
   var mongodb = require('mongodb');
   if (mongodb == null) return;
 
@@ -63,7 +104,7 @@ app.get('/', function (req, res) {
     initDb(function(err){});
   }
   if (db) {
-    var col = db.collection('counts');
+    var col = db.collection('counts'); // доступ к базе
     // Create a document with request IP and current time of request
     col.insert({ip: req.ip, date: Date.now()});
     col.count(function(err, count){
@@ -72,6 +113,71 @@ app.get('/', function (req, res) {
       }
       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
     });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
+});
+
+app.get('/remove', function (req, res) {
+  // try to initialize the db on every request if it's not already initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts'); // доступ к базе
+    col.remove({},function(err,numberRemoved){
+      res.send("inside remove call back" + numberRemoved);	
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
+});
+
+var str = "";
+app.get('/stat', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts'); // доступ к базе
+    // Create a document with request IP and current time of request
+    //col.insert({ip: req.ip, date: Date.now()});
+	var myobj = {ip: req.ip, date: Date.now()};
+	  
+	col.insertOne(myobj, function(err, res2) {
+	  if (err) throw err;
+	  //console.log("1 document inserted");
+	  // Get all the records in the test collection
+	  col.find({}).toArray(function(err, result) {
+		if (err) throw err;
+		str="<table border='1' cellpadding='7' style='border-collapse: collapse;'>";
+		str+="<caption style='color:blue; margin-bottom:15px;'>Содержание коллекции 'counts' базы данных Mongo 'db_nodejs_ex'</caption>";  
+		str+="<tr>";
+		str+="<th>ID</th>";  
+		str+="<th>IP</th>";
+		str+="<th>DATE</th>";
+		str+="</tr>";  
+		result.forEach(function(it, i, result) { 
+		  str+="<tr>";	
+		  for (var key in it) {
+		    if (it.hasOwnProperty(key)) {
+			  if(key=='date') {  
+			    var formatdate = timestamp_to_formatdate (it[key]); 
+				str+="<td>"+formatdate+"</td>";  
+			  } else {
+				str+="<td>"+it[key]+"</td>";  
+			  }	
+		    }
+		  }
+		  str+="</tr>";		
+		})
+		str+="</table>";
+		str+="<p style='color:green;'>" + "mongoURL: " + mongoURL + "</p>";  
+		res.send(str);
+	  }); // find	
+	}); // insert 
   } else {
     res.render('index.html', { pageCountMessage : null});
   }
@@ -94,7 +200,7 @@ app.get('/pagecount', function (req, res) {
 
 // error handling
 app.use(function(err, req, res, next){
-  console.error(err.stack);
+  console.error("err.stack:",err.stack);
   res.status(500).send('Something bad happened!');
 });
 
@@ -102,7 +208,25 @@ initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+app.listen(port, ipaddr);
+console.log('Server running on http://%s:%s', ipaddr, port);
 
-module.exports = app ;
+module.exports = app;
+
+function timestamp_to_formatdate (unix_timestamp) {
+  var dateTime = new Date(unix_timestamp);
+  var formatted = dateTime.toISOString();;	
+  return formatted;
+  var year = date.getFullYear();
+  var month = date.getMonth();
+  var date = date.getDate();	
+  // Hours part from the timestamp
+  var hours;// = date.getHours();
+  // Minutes part from the timestamp
+  var minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  var seconds = "0" + date.getSeconds();
+  // Will display time in 10:30:23 format
+  var formattedTime = date + "-" + month + "-" + year + " " + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);	
+  return formattedTime;	
+}	
